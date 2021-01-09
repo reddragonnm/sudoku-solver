81d87b154c115f7d06b9a3f3286d19e7ca1afd8e
Neelesh Mishra <neeleshojmishra@gmail.com>
2021-01-09 11:40:03 +0530

    Major updates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,123 @@
-function setup() {
+let solvedBoard;
+let selectedTile;
 
+let toSolve = false;
+let tileSize = 50;
+
+// let board = [
+//   [5, 3, 0, 0, 7, 0, 0, 0, 0],
+//   [6, 0, 0, 1, 9, 5, 0, 0, 0],
+//   [0, 9, 8, 0, 0, 0, 0, 6, 0],
+//   [8, 0, 0, 0, 6, 0, 0, 0, 3],
+//   [4, 0, 0, 8, 0, 3, 0, 0, 1],
+//   [7, 0, 0, 0, 2, 0, 0, 0, 6],
+//   [0, 6, 0, 0, 0, 0, 2, 8, 0],
+//   [0, 0, 0, 4, 1, 9, 0, 0, 5],
+//   [0, 0, 0, 0, 8, 0, 0, 7, 9],
+// ];
+
+let board;
+
+function initBoard() {
+  board = new Array(9);
+  for (let i = 0; i < 9; i++) {
+    board[i] = new Array(9);
+    for (let j = 0; j < 9; j++) {
+      board[i][j] = 0;
+    }
+  }
+}
+
+function resetBoard() {
+  initBoard();
+  toSolve = false;
+  selectedTile = null;
+  solvedBoard = null;
+}
+
+function mousePressed() {
+  let i = floor(map(mouseX, 0, width, 0, 9));
+  let j = floor(map(mouseY, 0, height, 0, 9));
+
+  if (0 <= i && i < 9 && 0 <= j && j < 9) {
+    selectedTile = [i, j];
+  }
+}
+
+function keyPressed() {
+  if (selectedTile) {
+    if ("123456789".includes(key))
+      board[selectedTile[1]][selectedTile[0]] = parseInt(key);
+
+    if (key == "Backspace" || key == "Delete")
+      board[selectedTile[1]][selectedTile[0]] = 0;
+  }
+}
+
+function showBoard(board) {
+  textSize(30);
+  textAlign(CENTER, CENTER);
+
+  stroke(0);
+  fill(0);
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (j % 3 == 0) strokeWeight(4);
+      else strokeWeight(1);
+      if (j != 0) line(j * tileSize, 0, j * tileSize, height);
+
+      if (i % 3 == 0) strokeWeight(4);
+      else strokeWeight(1);
+      if (i != 0) line(0, i * tileSize, width, i * tileSize);
+
+      stroke(0);
+      strokeWeight(1);
+      let t = board[i][j];
+      if (t != 0)
+        text(t, j * tileSize + tileSize / 2, i * tileSize + tileSize / 2);
+    }
+  }
+
+  if (selectedTile) {
+    let i = selectedTile[0];
+    let j = selectedTile[1];
+
+    noFill();
+    stroke("lightgreen");
+    strokeWeight(4);
+    rect(i * tileSize, j * tileSize, tileSize, tileSize);
+  }
+}
+
+function setup() {
+  let c = createCanvas(tileSize * 9, tileSize * 9);
+  select(".canvas").child(c);
+  initBoard();
+}
+
+function solveBoard(board, movesToDisplay) {
+  solve(board);
+
+  let histInterval = floor(hist.length / movesToDisplay);
+
+  let newHist = [];
+  for (let i = 0; i < hist.length; i += histInterval) {
+    newHist.push(hist[i]);
+  }
+  newHist.push(hist[hist.length - 1]);
+  hist = newHist;
+  toSolve = false;
 }
 
 function draw() {
+  background(255);
 
+  if (toSolve) solveBoard(board, 30);
+
+  if (hist.length > 0) {
+    solvedBoard = hist.shift();
+  }
+
+  if (solvedBoard) showBoard(solvedBoard);
+  else showBoard(board);
 }
---
+++ solver.js
@@ -0,0 +1,66 @@
+let hist = [];
+
+const getCol = (n) => board.map((x) => x[n]);
+
+function getSubBlock(a, b) {
+  a *= 3;
+  b *= 3;
+
+  let newB = [];
+  for (let i = a; i < a + 3; i++) {
+    for (let j = b; j < b + 3; j++) {
+      newB.push(board[i][j]);
+    }
+  }
+  return newB;
+}
+
+function isValid(num, i, j) {
+  if (board[i].includes(num)) return false;
+  if (getCol(j).includes(num)) return false;
+  if (getSubBlock(floor(i / 3), floor(j / 3)).includes(num)) return false;
+  return true;
+}
+
+function solve(board) {
+  let t = getEmpty(board);
+
+  if (t == null) {
+    return true;
+  }
+
+  let i = t[0];
+  let j = t[1];
+
+  for (let n = 1; n < 10; n++) {
+    if (isValid(n, i, j)) {
+      board[i][j] = n;
+      hist.push(copyBoard(board));
+
+      if (solve(board)) return true;
+
+      board[i][j] = 0;
+    }
+  }
+
+  return false;
+}
+
+function getEmpty(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] == 0) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+function copyBoard(board) {
+  let b2 = [];
+  for (let i = 0; i < 9; i++) {
+    b2.push([...board[i]]);
+  }
+  return b2;
+}
